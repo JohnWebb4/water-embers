c2959343a213d3eafbb758091f1acec2839caba8
JohnWebb4 <John.Webb94@gmail.com>
2018-05-25 15:08:47 -0700

    Create hello world using simple React

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ console.log('Port', app.get('port'));
 
 app.use(express.static('client/dist/'));
 
+app.get('/', (req, res) => { res.sendFile(__dirname + '/client/index.html'); });
+
 app.listen(app.get('port'), () => {
   console.log(`Listening on port ${app.get('port')}`);
 });
