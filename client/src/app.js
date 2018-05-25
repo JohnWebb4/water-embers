045c1326a97516361ab2e00896fb5e06b9ad3e5c
JohnWebb4 <John.Webb94@gmail.com>
2018-05-25 15:37:08 -0700

    Refactor App to React Component

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
 ReactDOM.render(
-  App,
+  <App />,
   document.getElementById('app')
 );
--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -1,3 +1,5 @@
-const App = (
-  <h1>Hello world</h1>
-);
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
