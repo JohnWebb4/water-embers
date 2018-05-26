a5c0552c743bcbb2835efa2edf949a53c448e53d
JohnWebb4 <John.Webb94@gmail.com>
2018-05-25 19:08:32 -0700

    Send mock data when requesting temperature

--- server/temperatureRouter.js
+++ server/temperatureRouter.js
@@ -1,7 +1,10 @@
 var router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send([{
+    date: Date.now(),
+    temperature: Math.random() * 20 + 70
+  }]);
 });
 
 module.exports = router;
