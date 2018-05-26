f90f03d6d803c99086373bd0ec24c09eb016c46d
JohnWebb4 <John.Webb94@gmail.com>
2018-05-25 18:57:09 -0700

    Route temperature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const temperatureRouter = require('./server/temperatureRouter.js');
 
 var app = express();
 
@@ -7,6 +8,8 @@ app.set('port', process.env.PORT || 8000);
 app.use(express.static('client/dist/'));
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/client/index.html'); });
+
+app.use('/temperature', temperatureRouter);
 
 app.listen(app.get('port'), () => {
   console.log(`Listening on port ${app.get('port')}`);
