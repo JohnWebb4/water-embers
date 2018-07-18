8f8ae3b2126af5666a7e4dd9dc04730578eeac39
JohnWebb4 <John.Webb94@gmail.com>
2018-07-17 17:02:35 -0700

    Linte App

--- client/dist/components/App.js
+++ client/dist/components/App.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -26,7 +26,7 @@ var App = function (_React$Component) {
   }
 
   _createClass(App, [{
-    key: 'componentDidMount',
+    key: "componentDidMount",
     value: function componentDidMount() {
       var _this2 = this;
 
@@ -37,28 +37,29 @@ var App = function (_React$Component) {
       }, 10000);
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
       return React.createElement(
         React.Fragment,
         null,
         React.createElement(
-          'h1',
+          "h1",
           null,
-          'Current Temperature'
+          "Current Temperature"
         ),
         React.createElement(Temperature, { temperature: this.state.currentTemperature }),
         React.createElement(Search, { onSearch: this.search }),
+        React.createElement("canvas", { id: "resultPlot" }),
         React.createElement(
-          'h3',
+          "h3",
           null,
-          'Search Results'
+          "Search Results"
         ),
         React.createElement(TemperaturesList, { temperatures: this.state.temperatures })
       );
     }
   }, {
-    key: 'fetchCurrentTemperature',
+    key: "fetchCurrentTemperature",
     value: function fetchCurrentTemperature() {
       var _this3 = this;
 
@@ -71,11 +72,11 @@ var App = function (_React$Component) {
       });
     }
   }, {
-    key: 'search',
+    key: "search",
     value: function search(value) {
       var _this4 = this;
 
-      fetch('/temperature?search=' + value.search + '&numResults=' + value.numResults).then(function (res) {
+      fetch("/temperature?search=" + value.search + "&numResults=" + value.numResults).then(function (res) {
         return res.json();
       }).then(function (temperatures) {
         _this4.setState({
